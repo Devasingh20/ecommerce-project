28abdc0cba72ef78b56e991f19c170f35c414170
Deva Singh <deva81002@gmail.com>
2026-08-15 14:55:37 +0530

    create multiple pages react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
+import { Routes, Route } from 'react-router'
 import './App.css'
 import { HomePage } from './Pages/HomePage.jsx'
 
 function App() {
 
   return (
-    HomePage()
+    <Routes>
+      <Route
+        index
+        element={<HomePage />} />
+
+
+      <Route
+        path="checkout"
+        element={<h1>Hello,Mr. Deva</h1>} />
+    </Routes>
   )
 }
 
